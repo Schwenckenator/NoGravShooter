cd3f50a48500b0d2df68a7fe9a29406c0dfaa390
Nathan <nathanjbrown@live.com>
2015-02-05 11:40:58 +1000

    EMP glitching
    seems to work over network now

--- Unity Project/Assets/$Scripts/Gameplay/PlayerResources.cs
+++ Unity Project/Assets/$Scripts/Gameplay/PlayerResources.cs
@@ -429,19 +429,15 @@ public class PlayerResources : MonoBehaviour {
 	public void EMPglitch(){
 		glitched = true;
 	}
-	public bool glitched = false;
-	/* EMP glitchyness did not like networking, was weird though,
-	if one player got hits by the blast sometimes it was every screen,
-	sometimes it was 2 out of 4, othertimes just the one that was hit.
-	
+	private bool glitched = false;
 	void OnGUI(){
 		if(!glitched) return;
-		if(!GameManager.IsPaused()){
+		if(!GameManager.IsPaused() && networkView.isMine){
 			GUI.depth = 0;
 			GUI.DrawTexture(new Rect(20+Random.Range(-10, 11), Screen.height-240+Random.Range(-10, 11), 220, 220), gameManager.GetComponent<GuiManager>().EMPradar[Random.Range(0, 5)]);
 			GUI.DrawTexture(new Rect(Screen.width/2 - 55/2, Screen.height/2 - 45/2, 55, 45), gameManager.GetComponent<GuiManager>().EMPcursor[Random.Range(0, 4)]);
 			GUI.DrawTexture(new Rect(Screen.width-265, Screen.height-235+Random.Range(-5, 6), 265, 235), gameManager.GetComponent<GuiManager>().EMPstats[Random.Range(0, 4)]);
 		}
-	}*/
+	}
 
 }
