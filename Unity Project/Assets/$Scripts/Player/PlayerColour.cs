cdf748c2686809b792c6bcb8cd69531e31a2c1ab
Nathan <nathanjbrown@live.com>
2015-03-04 21:48:35 +1000

    redvsblue colours aka allow multiple shades in team
    was just trying something out, we can always change it back later

--- Unity Project/Assets/$Scripts/Player/PlayerColour.cs
+++ Unity Project/Assets/$Scripts/Player/PlayerColour.cs
@@ -24,19 +24,24 @@ public class PlayerColour : MonoBehaviour {
         networkView.RPC("RPCAssignPlayerColour", RPCMode.AllBuffered, settingsManager.ColourR, settingsManager.ColourG, settingsManager.ColourB);
     }
     private void ApplyTeamColour(Team team) {
-        float red = 0;
-        float green = 0;
-        float blue = 0;
+        settingsManager = GameObject.FindGameObjectWithTag("GameController").GetComponent<SettingsManager>();
+        float red = settingsManager.ColourR;
+        float green = settingsManager.ColourG;
+        float blue = settingsManager.ColourB;
+		//just an idea i had while watching redvsblue, can always take it out later
+		//allows multiple shades while keeping within the general colour pallet of the team
         switch (team) {
             case Team.Red:
-                red = 1;
-                green = 0;
-                blue = 0;
+				//limits colours to red(duh), light red, dark red, pink and orange
+				if(red < 0.85f){red = 0.85f;}
+				if(green > 0.5f){green = 0.5f;}
+				if(blue > 0.5f){blue = 0.5f;}
                 break;
             case Team.Blue:
-                red = 0;
-                green = 0;
-                blue = 1;
+				//limits colours to blue(duh), light blue, dark blue, purple and teal
+				if(red > 0.5f){red = 0.5f;}
+				if(green > 0.75f){green = 0.75f;}
+				if(blue < 0.85){blue = 0.85f;}
                 break;
         }
         networkView.RPC("RPCAssignPlayerColour", RPCMode.AllBuffered, red, green, blue);
