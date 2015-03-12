1520179ea11257aeff184d34b5164e425f741678
Schwenckenator <matt.schwencke@gmail.com>
2015-03-12 23:03:39 +1000

    Fixed assassination bug.
    Assassination bug with everyone seeing the assassination prompt is
    fixed. Probably.

--- Unity Project/Assets/$Scripts/Player/PlayerIsMine.cs
+++ Unity Project/Assets/$Scripts/Player/PlayerIsMine.cs
@@ -6,12 +6,10 @@ public class PlayerIsMine : MonoBehaviour {
 	void Awake(){
 		if(!networkView.isMine){
 			GetComponent<NoGravCharacterMotor>().enabled = false;
-
 			GetComponent<MouseLook>().enabled = false;
-
 			transform.FindChild("CameraPos").GetComponent<MouseLook>().enabled = false;
-
 			GetComponentInChildren<FireWeapon>().enabled = false;
+            GetComponent<Assassinations>().enabled = false;
 		}
 	}
 }
