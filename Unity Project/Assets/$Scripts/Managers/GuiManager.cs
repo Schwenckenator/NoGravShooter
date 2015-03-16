9ff9b7d0ee5cf3a527481fb467558bd5e86e8906
Schwenckenator <matt.schwencke@gmail.com>
2015-03-16 16:54:15 +1000

    Commented all Colour preview code.
    Removed all texture code. Game seems to work silky smooth now.

--- Unity Project/Assets/$Scripts/Managers/GuiManager.cs
+++ Unity Project/Assets/$Scripts/Managers/GuiManager.cs
@@ -123,14 +123,14 @@ public class GuiManager : MonoBehaviour {
 	GUIStyle RedTeamFont;
 	GUIStyle BlueTeamFont;
 	
-	private Color defaultcol;
-	private Texture2D coltexture;
-	private Color playercol;
-	private Texture2D playertexture;
-	private Color redTeamColour;
-	private Texture2D redTeamTexture;
-	private Color blueTeamColour;
-	private Texture2D blueTeamTexture;
+    //private Color defaultcol;
+    //private Texture2D coltexture;
+    //private Color playercol;
+    //private Texture2D playertexture;
+    //private Color redTeamColour;
+    //private Texture2D redTeamTexture;
+    //private Color blueTeamColour;
+    //private Texture2D blueTeamTexture;
 
     #endregion
 
@@ -167,11 +167,11 @@ public class GuiManager : MonoBehaviour {
 		BlueTeamFont.alignment = TextAnchor.UpperLeft;
 		BlueTeamFont.fontSize = 16;
 		
-		defaultcol = new Color (0.1f, 0.1f, 0.1f, 0.5f);
-		coltexture = new Texture2D(1, 1);
-		coltexture.SetPixel(0,0,defaultcol);
-		coltexture.Apply();
-		GUI.skin.box.normal.background = coltexture;
+        //defaultcol = new Color (0.1f, 0.1f, 0.1f, 0.5f);
+        //coltexture = new Texture2D(1, 1);
+        //coltexture.SetPixel(0,0,defaultcol);
+        //coltexture.Apply();
+        //GUI.skin.box.normal.background = coltexture;
 		//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
         menuText = SettingsManager.instance.DisplayServerName + ", " + SettingsManager.instance.GameModeName;
@@ -453,33 +453,33 @@ public class GuiManager : MonoBehaviour {
     private float red = -99;
     private float green = -99;
     private float blue = -99;
-    void UpdateCustomColours(int colour) {
-        if (colour == 0) {
-            redTeamColour = SettingsManager.instance.GetPlayerColour();
-            redTeamColour = PlayerColourManager.instance.LimitTeamColour(TeamColour.Red, redTeamColour);
-            redTeamTexture = new Texture2D(1, 1);
-            redTeamTexture.SetPixel(0, 0, redTeamColour);
-            redTeamTexture.Apply();
-            red = SettingsManager.instance.ColourR;
-        } else if (colour == 1) {
-            playercol = SettingsManager.instance.GetPlayerColour();
-            playercol = PlayerColourManager.instance.LimitTeamColour(TeamColour.None, playercol);
-            playertexture = new Texture2D(1, 1);
-            playertexture.SetPixel(0, 0, playercol);
-            playertexture.Apply();
-            green = SettingsManager.instance.ColourG;
-        } else {
-            blueTeamColour = SettingsManager.instance.GetPlayerColour();
-            blueTeamColour = PlayerColourManager.instance.LimitTeamColour(TeamColour.Blue, blueTeamColour);
-            blueTeamTexture = new Texture2D(1, 1);
-            blueTeamTexture.SetPixel(0, 0, blueTeamColour);
-            blueTeamTexture.Apply();
-            blue = SettingsManager.instance.ColourB;
-        }
-    }
-    bool ColoursChanged() {
-        return (red != SettingsManager.instance.ColourR || green != SettingsManager.instance.ColourG || blue != SettingsManager.instance.ColourB);
-    }
+    //void UpdateCustomColours(int colour) {
+    //    if (colour == 0) {
+    //        redTeamColour = SettingsManager.instance.GetPlayerColour();
+    //        redTeamColour = PlayerColourManager.instance.LimitTeamColour(TeamColour.Red, redTeamColour);
+    //        redTeamTexture = new Texture2D(1, 1);
+    //        redTeamTexture.SetPixel(0, 0, redTeamColour);
+    //        redTeamTexture.Apply();
+    //        red = SettingsManager.instance.ColourR;
+    //    } else if (colour == 1) {
+    //        playercol = SettingsManager.instance.GetPlayerColour();
+    //        playercol = PlayerColourManager.instance.LimitTeamColour(TeamColour.None, playercol);
+    //        playertexture = new Texture2D(1, 1);
+    //        playertexture.SetPixel(0, 0, playercol);
+    //        playertexture.Apply();
+    //        green = SettingsManager.instance.ColourG;
+    //    } else {
+    //        blueTeamColour = SettingsManager.instance.GetPlayerColour();
+    //        blueTeamColour = PlayerColourManager.instance.LimitTeamColour(TeamColour.Blue, blueTeamColour);
+    //        blueTeamTexture = new Texture2D(1, 1);
+    //        blueTeamTexture.SetPixel(0, 0, blueTeamColour);
+    //        blueTeamTexture.Apply();
+    //        blue = SettingsManager.instance.ColourB;
+    //    }
+    //}
+    //bool ColoursChanged() {
+    //    return (red != SettingsManager.instance.ColourR || green != SettingsManager.instance.ColourG || blue != SettingsManager.instance.ColourB);
+    //}
 
 	void OptionsWindow(int windowId){
 		Rect standard = new Rect(20, 20, -40+Screen.width/3, 30);
@@ -493,11 +493,11 @@ public class GuiManager : MonoBehaviour {
         SettingsManager.instance.ColourR = float.Parse(GUI.TextField(new Rect(standard.x, standard.y, 50, 20), SettingsManager.instance.ColourR.ToString()));
 
 		// Display Red team Preview
-		if(ColoursChanged()){
-			UpdateCustomColours(0);
-		}
+        //if(ColoursChanged()){
+        //    UpdateCustomColours(0);
+        //}
 		GUI.Label(new Rect(standard.x, standard.y+30, 70, 20), "Red Team");
-        GUI.skin.box.normal.background = redTeamTexture;
+        //GUI.skin.box.normal.background = redTeamTexture;
 		GUI.Box(new Rect(standard.x, standard.y+60, 50, 30), GUIContent.none);
         SettingsManager.instance.ColourR = float.Parse(GUI.TextField(new Rect(standard.x, standard.y, 50, 20), SettingsManager.instance.ColourR.ToString()));
         standard.x -= 20;
@@ -512,12 +512,12 @@ public class GuiManager : MonoBehaviour {
 		standard.y += 20;
         SettingsManager.instance.ColourG = float.Parse(GUI.TextField(new Rect(standard.x, standard.y, 50, 20), SettingsManager.instance.ColourG.ToString()));
 
-        //Display No team Preview
-        if (ColoursChanged()) {
-			UpdateCustomColours(1);
-		}
+        ////Display No team Preview
+        //if (ColoursChanged()) {
+        //    UpdateCustomColours(1);
+        //}
 		GUI.Label(new Rect(standard.x, standard.y+30, 70, 20), "Default");
-		GUI.skin.box.normal.background = playertexture;
+        //GUI.skin.box.normal.background = playertexture;
 		GUI.Box(new Rect(standard.x, standard.y+60, 50, 30), GUIContent.none);
         SettingsManager.instance.ColourG = float.Parse(GUI.TextField(new Rect(standard.x, standard.y, 50, 20), SettingsManager.instance.ColourG.ToString()));
 		if(GUI.Button(new Rect(standard.x, standard.y+100, 70, 30), "Random")){
@@ -538,17 +538,17 @@ public class GuiManager : MonoBehaviour {
         SettingsManager.instance.ColourB = float.Parse(GUI.TextField(new Rect(standard.x, standard.y, 50, 20), SettingsManager.instance.ColourB.ToString()));
 		
         // Display Blue team Preview
-        if (ColoursChanged()) {
-			UpdateCustomColours(2);
-		}		
+        //if (ColoursChanged()) {
+        //    UpdateCustomColours(2);
+        //}		
 		GUI.Label(new Rect(standard.x, standard.y+30, 70, 20), "Blue Team");
-        GUI.skin.box.normal.background = blueTeamTexture;
+        //GUI.skin.box.normal.background = blueTeamTexture;
 		GUI.Box(new Rect(standard.x, standard.y+60, 50, 30), GUIContent.none);
         SettingsManager.instance.ColourB = float.Parse(GUI.TextField(new Rect(standard.x, standard.y, 50, 20), SettingsManager.instance.ColourB.ToString()));
 		standard.x -= 20;
 		SettingsManager.instance.ColourB = GUI.VerticalSlider(new Rect(standard.x, standard.y, 20, 200), SettingsManager.instance.ColourB, 1, 0);
 		
-        GUI.skin.box.normal.background = coltexture;
+        //GUI.skin.box.normal.background = coltexture;
 		
 		standard.x -= standard.width+250;
 		standard.y += 160;
