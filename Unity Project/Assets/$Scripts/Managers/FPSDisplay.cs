6b806a4297a89b187d8213db50c4a15010fe068b
Schwenckenator <matt.schwencke@gmail.com>
2015-03-23 21:18:13 +1000

    Added FPS counter.
    FPS counter added. Always showing for now, but there is a way to disable
    it.
    Also changed the version numbering from 1.0 to 0.1
    Calling our current build version 1 is a bit presumptuous isn't it?

---
+++ Unity Project/Assets/$Scripts/Managers/FPSDisplay.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPSDisplay : MonoBehaviour {
+
+    public float updateRate = 4.0f;
+    public bool isDisplayFPS = true;
+
+    int frameCount = 0;
+    float nextUpdate = 0.0f;
+    float fps = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+        nextUpdate = Time.time;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        CountFrame();
+	}
+
+    void OnGUI() {
+        if (!isDisplayFPS) return;
+
+        if (fps > 60) {
+            GUI.contentColor = Color.white;
+        } else if (fps > 30) {
+            GUI.contentColor = Color.yellow;
+        } else {
+            GUI.contentColor = Color.red;
+        }
+
+        GUI.Label(new Rect(Screen.width - 100, 40, 100, 20), "FPS: " + fps.ToString());
+    }
+
+    private void CountFrame() {
+        frameCount++;
+        if (Time.time > nextUpdate) {
+            nextUpdate += 1.0f / updateRate;
+            fps = frameCount * updateRate;
+            frameCount = 0;
+        }
+    }
+}
