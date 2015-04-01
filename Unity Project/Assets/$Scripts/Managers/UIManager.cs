4fb206f5d6ca6f42fc261b3e80e89b562038644b
Schwenckenator <matt.schwencke@gmail.com>
2015-04-02 04:01:52 +1000

    Canvas city
    Many Canvases. More prefabs. Nothing works.

--- Unity Project/Assets/$Scripts/Managers/UIManager.cs
+++ Unity Project/Assets/$Scripts/Managers/UIManager.cs
@@ -1,5 +1,6 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class UIManager : MonoBehaviour {
     #region Instance
@@ -18,9 +19,45 @@ public class UIManager : MonoBehaviour {
     }
     #endregion
 
-    public enum Menu { MainMenu, CreateGame, JoinGame, Options, Quit, Lobby, GameSettings, JoinByIP, Connecting, Keybind, ChangeKeybind, GraphicsOptions, PasswordInput }
+    public enum Menu { MainMenu = 0, CreateGame, JoinGame, Options, Quit, Lobby, GameSettings, JoinByIP, Connecting, Keybind, ChangeKeybind, GraphicsOptions, PasswordInput }
+    public GameObject[] menus; // Only for initialisation
+    private static List<Canvas> window;
+    private static int currentWindow;
 
+    void Start() {
+        window = new List<Canvas>();
+
+        SetCurrentWindow(Menu.MainMenu);
+
+        foreach (GameObject menu in menus) {
+            //Create, then hide menu windows
+            GameObject newMenu = Instantiate(menu) as GameObject;
+            Canvas newCanvas = newMenu.GetComponent<Canvas>();
+            newCanvas.enabled = false;
+            window.Add(newCanvas);
+        }
+        //Enable Main menu
+        window[0].enabled = true;
+    }
+    #region SetWindow
+    private static void SetCurrentWindow(Menu menu){
+        // Only changes currentWindow
+        currentWindow = (int)menu;
+    }
     public void SetMenuWindow(Menu newWindow) {
+        SetMenuWindow((int)newWindow);
+    }
+    public void SetMenuWindow(int newWindow) {
+        window[currentWindow].enabled = false;
+        window[newWindow].enabled = true;
 
+        currentWindow = newWindow;
+    }
+    #endregion
+
+    public void QuitGame() {
+        if (!Application.isWebPlayer && !Application.isEditor) {
+            Application.Quit();
+        }
     }
 }
