dfcfeab9a512b2df30e50b3b186d95aaff73ed4a
Schwenckenator <matt.schwencke@gmail.com>
2015-04-02 01:26:37 +1000

    GUI Stuff
    Start of a new beginning.

---
+++ Unity Project/Assets/$Scripts/Managers/UIManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UIManager : MonoBehaviour {
+    #region Instance
+    //Here is a private reference only this class can access
+    private static UIManager _instance;
+    //This is the public reference that other classes will use
+    public static UIManager instance {
+        get {
+            //If _instance hasn't been set yet, we grab it from the scene!
+            //This will only happen the first time this reference is used.
+            if (_instance == null) {
+                _instance = GameObject.FindObjectOfType<UIManager>();
+            }
+            return _instance;
+        }
+    }
+    #endregion
+
+    public enum Menu { MainMenu, CreateGame, JoinGame, Options, Quit, Lobby, GameSettings, JoinByIP, Connecting, Keybind, ChangeKeybind, GraphicsOptions, PasswordInput }
+
+    public void SetMenuWindow(Menu newWindow) {
+
+    }
+}
