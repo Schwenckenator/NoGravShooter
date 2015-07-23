89d8181d07cd9d7591b9d38b3112cd0d7c7791d6
Nathan <nathanjbrown@live.com>
2015-07-23 14:29:41 +1000

    added weak melee attack

--- ZeroG/Assets/$Scripts/Player/Assassinations.cs
+++ ZeroG/Assets/$Scripts/Player/Assassinations.cs
@@ -26,26 +26,41 @@ public class Assassinations : MonoBehaviour {
         heading = transform.position - target.position;
         Vector3 toTarget = (heading).normalized;
         Vector3 toPlayer = (target.position - transform.position).normalized;
-        //checks if player is behind another player and is close enough to melee
-        //kinda dodgy but it works, dont ask where the 12 came from
-        if (Vector3.Dot(toTarget, transform.forward) < -0.75 && heading.sqrMagnitude < detectionRadius * detectionRadius / 12) { // <-- where did that twelve come from? Magic numbers are bad yo
+        //checks if player is close enough to melee
+        if (heading.sqrMagnitude < detectionRadius * detectionRadius) {
             CheckFacing(hit, toPlayer);
         }
     }
 
     private void CheckFacing(Collider hit, Vector3 toPlayer) {
+        target = hit.transform;
+        heading = transform.position - target.position;
+        Vector3 toTarget = (heading).normalized;
         playerResource = hit.gameObject.GetInterface<IDamageable>();
-        //checks if the player is actually facing the player they want to assassinate
-        if (Vector3.Dot(toPlayer, target.forward) > 0.75 && playerResource.GetHealth() > 0) {
-            //GuiManager.instance.ButtonPrompt("Assassinate", (int)KeyBind.Interact);
-            UIPlayerHUD.Prompt(InputConverter.GetKeyName(KeyBind.Interact) + " - Assassinate");
-            Assassinate();
+        //checks if the player is actually facing the player they want to melee
+        if (Vector3.Dot(toTarget, transform.forward) < -0.75 && playerResource.GetHealth() > 0) {
+            //checks if the player is behind the player they want to melee
+			if (Vector3.Dot(toPlayer, target.forward) > 0.75) {
+				UIPlayerHUD.Prompt(InputConverter.GetKeyName(KeyBind.Interact) + " - Assassinate");
+				Assassinate();
+			} else {
+				UIPlayerHUD.Prompt(InputConverter.GetKeyName(KeyBind.Interact) + " - Melee");
+				MeleeAttack(hit);
+			}
         }
     }
 
     private void Assassinate() {
         if (InputConverter.GetKey(KeyBind.Interact)) {
             playerResource.TakeDamage(100, Network.player, 200); // 200 is assassination ID
+        }
+    }
+	private float meleecooldown = 0f;
+    private void MeleeAttack(Collider hit) {
+        if (InputConverter.GetKey(KeyBind.Interact) && Time.time > meleecooldown) {
+            playerResource.TakeDamage(10, Network.player, 200);
+			hit.gameObject.GetComponent<Rigidbody>().AddForce(transform.forward * 2, ForceMode.Impulse);
+			meleecooldown = Time.time + 0.5F;;
         }
     }
 }
