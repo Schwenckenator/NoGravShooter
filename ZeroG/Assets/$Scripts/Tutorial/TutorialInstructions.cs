7cb57a174653d7d11a511d06f6eb6c1d26811480
Nathan <nathanjbrown@live.com>
2015-07-25 23:53:26 +1000

    Tutorial Checklist added

--- ZeroG/Assets/$Scripts/Tutorial/TutorialInstructions.cs
+++ ZeroG/Assets/$Scripts/Tutorial/TutorialInstructions.cs
@@ -91,33 +91,45 @@ public class TutorialInstructions : MonoBehaviour {
             dotLooks++;
         }
         if (dotLooks == 1) {
+			updateCheckList(1, true);
             lookingDot.transform.position = new Vector3(-358f, -10.6f, -3f);
         }
         if (dotLooks == 2) {
+			updateCheckList(2, true);
             lookingDot.transform.position = new Vector3(-358f, -7.6f, 0f);
         }
         if (dotLooks == 3) {
+			updateCheckList(3, true);
             lookingDot.transform.position = new Vector3(-358f, -10.6f, 3f);
         }
         if (dotLooks == 4) {
+			updateCheckList(4, true);
             lookingDot.transform.position = new Vector3(-358f, -13.6f, 0f);
         }
+        if (dotLooks == 5) {
+			updateCheckList(5, true);
+        }
+		
 
         if (check1) {
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.MoveForward])) {
                 F = true;
+				updateCheckList(1, true);
                 Debug.Log("Player Moved Forwards");
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.MoveRight])) {
                 R = true;
+				updateCheckList(3, true);
                 Debug.Log("Player Moved Right");
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.MoveBack])) {
                 B = true;
+				updateCheckList(4, true);
                 Debug.Log("Player Moved Backwards");
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.MoveLeft])) {
                 L = true;
+				updateCheckList(2, true);
                 Debug.Log("Player Moved Left");
             }
         }
@@ -126,14 +138,17 @@ public class TutorialInstructions : MonoBehaviour {
             if (Input.GetMouseButtonDown(0)) {
                 shot = true;
                 Debug.Log("Player Shot");
+				updateCheckList(1, true);
             }
             if (Input.GetMouseButtonDown(1)) {
                 aimed = true;
                 Debug.Log("Player Aimed");
+				updateCheckList(2, true);
             }
             if (Input.GetKeyDown("2") || Input.GetAxis("Mouse ScrollWheel") < 0 || Input.GetAxis("Mouse ScrollWheel") > 0) {
                 changedgun = true;
                 Debug.Log("Player Changed Weapons");
+				updateCheckList(3, true);
             }
         }
 
@@ -141,14 +156,17 @@ public class TutorialInstructions : MonoBehaviour {
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.JetUp])) {
                 U = true;
                 Debug.Log("Player Flew Up");
+				updateCheckList(1, true);
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.JetDown])) {
                 D = true;
                 Debug.Log("Player Flew Down");
+				updateCheckList(2, true);
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.StopMovement])) {
                 X = true;
                 Debug.Log("Player Braked");
+				updateCheckList(3, true);
             }
         }
 
@@ -156,10 +174,12 @@ public class TutorialInstructions : MonoBehaviour {
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.RollLeft])) {
                 RL = true;
                 Debug.Log("Player Rolled Left");
+				updateCheckList(1, true);
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.RollRight])) {
                 RR = true;
                 Debug.Log("Player Rolled Right");
+				updateCheckList(2, true);
             }
         }
 
@@ -167,10 +187,12 @@ public class TutorialInstructions : MonoBehaviour {
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.Grenade])) {
                 grenade = true;
                 Debug.Log("Player Threw Grenade");
+				updateCheckList(1, true);
             }
             if (Input.GetKeyDown(SettingsManager.keyBindings[(int)KeyBind.GrenadeSwitch])) {
                 changedgrenade = true;
                 Debug.Log("Player Changed Grenade-type");
+				updateCheckList(2, true);
             }
         }
 
@@ -187,12 +209,20 @@ public class TutorialInstructions : MonoBehaviour {
             } else {
                 shotAllTargets = true;
                 Debug.Log("Player shot all targets");
+				updateCheckList(1, true);
             }
         }
 
+		if (landedPlatforms == 1){
+			updateCheckList(1, true);
+		}
+		if (landedPlatforms == 2){
+			updateCheckList(2, true);
+		}
         if (landedPlatforms > 2) {
             platforms = true;
             Debug.Log("Player landed on all platforms");
+			updateCheckList(3, true);
             landedPlatforms = 0; // Fix message Spam
         }
 
@@ -206,19 +236,24 @@ public class TutorialInstructions : MonoBehaviour {
             step1 = false;
             StartCoroutine(MovementTutorial2());
         }
-        if (Floor1) {
-            litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
-            litfloorpanel = GameObject.Find("walkhere2");
-            litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
-        }
-        if (Floor2) {
-            litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
-            litfloorpanel = GameObject.Find("walkhere3");
-            litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
-        }
-        if (Floor3) {
-            litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
-        }
+		if(step2){
+			if (Floor1) {
+				litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
+				litfloorpanel = GameObject.Find("walkhere2");
+				litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+					updateCheckList(1, true);
+			}
+			if (Floor2) {
+				litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
+				litfloorpanel = GameObject.Find("walkhere3");
+				litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
+					updateCheckList(2, true);
+			}
+			if (Floor3) {
+				litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 0, 1);
+					updateCheckList(3, true);
+			}
+		}
         if (Floor1 && Floor2 && Floor3 && step2) {
             step2 = false;
             openroom2 = true;
@@ -311,6 +346,12 @@ public class TutorialInstructions : MonoBehaviour {
         lookingDot.transform.position = new Vector3(-358f, -10.6f, 0f);
         ChatManager.TutorialChat("\nPlease look at the blue dot.");
 		UIPlayerHUD.TutorialPrompt("\nMove your Mouse to look around.\n\n\n\n\nPress Space to continue.");
+		CheckList(5);
+		updateCheckList(1, false, "Look at blue dot 1");
+		updateCheckList(2, false, "Look at blue dot 2");
+		updateCheckList(3, false, "Look at blue dot 3");
+		updateCheckList(4, false, "Look at blue dot 4");
+		updateCheckList(5, false, "Look at blue dot 5");
         yield return new WaitForSeconds(5);
         step0 = true;
     }
@@ -324,19 +365,30 @@ public class TutorialInstructions : MonoBehaviour {
             + SettingsManager.keyBindings[(int)KeyBind.MoveLeft].ToString() + " to move Left.\n\nPress "
             + SettingsManager.keyBindings[(int)KeyBind.MoveRight].ToString() + " to move Right.\n\nPress "
             + SettingsManager.keyBindings[(int)KeyBind.MoveBack].ToString() + " to move Backwards.\n\n\n\n\nPress Space to continue.");
+		CheckList(4);
+		updateCheckList(1, false, "Move forwards");
+		updateCheckList(2, false, "Move left");
+		updateCheckList(3, false, "Move right");
+		updateCheckList(4, false, "Move backwards");
         yield return new WaitForSeconds(4);
         step1 = true;
     }
     IEnumerator MovementTutorial2() {
+        check1 = false;
         checkingfloortiles = true;
         litfloorpanel = GameObject.Find("walkhere1");
         litfloorpanel.transform.GetComponent<Renderer>().material.color = new Color(0, 1, 0);
         ChatManager.TutorialChat("\nWalk onto the green squares.");
+		CheckList(3);
+		updateCheckList(1, false, "Move to green sqaure 1");
+		updateCheckList(2, false, "Move to green sqaure 2");
+		updateCheckList(3, false, "Move to green sqaure 3");
         yield return new WaitForSeconds(5);
         step2 = true;
     }
 
     IEnumerator GunTutorial() {
+        checkingfloortiles = false;
         ChatManager.TutorialChat("\nWell done!\n\nPlease proceed to the next room.");
         yield return new WaitForSeconds(7);
         check3 = true;
@@ -344,18 +396,26 @@ public class TutorialInstructions : MonoBehaviour {
         yield return new WaitForSeconds(10);
         UIPlayerHUD.TutorialPrompt("\nClick the left Mouse Button to shoot and use the right Mouse Button to aim.\n\nUse the Mouse Wheel or Numbers to change weapons.\n\nPress "
         + SettingsManager.keyBindings[(int)KeyBind.Reload].ToString() + " to reload.\n\n\n\n\nPress Space to continue.");
+		CheckList(3);
+		updateCheckList(1, false, "Shoot a gun");
+		updateCheckList(2, false, "Aim a gun");
+		updateCheckList(3, false, "Change guns");
         yield return new WaitForSeconds(5);
         step3 = true;
     }
 
     IEnumerator GunTutorial2() {
+        check3 = false;
         checkingtargets = true;
         ChatManager.TutorialChat("\nTry to shoot all the targets.");
+		CheckList(1);
+		updateCheckList(1, false, "Shoot the targets");
         yield return new WaitForSeconds(5);
         step4 = true;
     }
 
     IEnumerator FlightTutorial() {
+        checkingtargets = false;
         ChatManager.TutorialChat("\nWell done!\n\nPlease proceed to the next room.");
         yield return new WaitForSeconds(7);
         check5 = true;
@@ -365,10 +425,15 @@ public class TutorialInstructions : MonoBehaviour {
 			+SettingsManager.keyBindings[(int)KeyBind.StopMovement].ToString()+" to brake.");
         UIPlayerHUD.TutorialPrompt("\nPress "+SettingsManager.keyBindings[(int)KeyBind.JetUp].ToString()+" to boost up.\n\nPress "+SettingsManager.keyBindings[(int)KeyBind.JetDown].ToString()
 		+" to boost down.\n\nPress "+SettingsManager.keyBindings[(int)KeyBind.StopMovement].ToString()+" to brake.\n\n\n\n\nPress Space to continue.");
+		CheckList(3);
+		updateCheckList(1, false, "Fly up");
+		updateCheckList(2, false, "Fly down");
+		updateCheckList(3, false, "Use brakes");
         yield return new WaitForSeconds(10);
         step5 = true;
     }
     IEnumerator FlightTutorial2() {
+        check5 = false;
         check6 = true;
         ChatManager.TutorialChat("\nPress "
             + SettingsManager.keyBindings[(int)KeyBind.RollLeft].ToString() + " & "
@@ -377,11 +442,15 @@ public class TutorialInstructions : MonoBehaviour {
             + SettingsManager.keyBindings[(int)KeyBind.RollLeft].ToString() + " to roll to the left and "
             + SettingsManager.keyBindings[(int)KeyBind.RollRight].ToString() + " to roll to the right.\n\n"
 			+ "You can also rotate by moving the mouse while floating.\n\n\n\n\nPress Space to continue.");
+		CheckList(2);
+		updateCheckList(1, false, "Roll to the left");
+		updateCheckList(2, false, "Roll to the right");
         yield return new WaitForSeconds(5);
         step6 = true;
     }
 
     IEnumerator NavigationTutorial() {
+        check6 = false;
         platform1 = GameObject.Find("LandingPlatform1");
         platform2 = GameObject.Find("LandingPlatform2");
         platform3 = GameObject.Find("LandingPlatform3");
@@ -390,6 +459,10 @@ public class TutorialInstructions : MonoBehaviour {
         platform3.transform.position = new Vector3(platform3.transform.position.x, 0, platform3.transform.position.z);
         ChatManager.TutorialChat("\nTry to land on the green platforms.\n\nRemember to 'look up' before you crash so you land feet first.");
         UIPlayerHUD.TutorialPrompt("\nTry to land on the green platforms.\n\nRemember to 'look up' before you crash so you land feet first.\n\n\n\n\nPress Space to continue.");
+		CheckList(3);
+		updateCheckList(1, false, "Land on platform 1");
+		updateCheckList(2, false, "Land on platform 2");
+		updateCheckList(3, false, "Land on platform 3");
         yield return new WaitForSeconds(5);
         step7 = true;
     }
@@ -406,14 +479,19 @@ public class TutorialInstructions : MonoBehaviour {
             + SettingsManager.keyBindings[(int)KeyBind.Grenade].ToString() + " to throw a Proximity Grenade.\n\nPress " 
 			+ SettingsManager.keyBindings[(int)KeyBind.GrenadeSwitch].ToString() 
 			+ " to change grenade type.\n\nKeep in mind that without gravity, the grenades will fly in a straight line.\n\n\n\n\nPress Space to continue.");
+		CheckList(2);
+		updateCheckList(1, false, "Throw a grenade");
+		updateCheckList(2, false, "Change grenade type");
         yield return new WaitForSeconds(8);
         step8 = true;
     }
 
     IEnumerator ItemTutorial() {
+        check8 = false;
         ChatManager.TutorialChat(
             "\nIf a grenade hits a person it will detonate instantly.\n\nYou can also set off grenades by shooting them.");
         yield return new WaitForSeconds(8);
+		CheckList(0);
         healthpack = GameObject.Find("TutorialBonusHealthPack");
         healthpack.transform.position = new Vector3(142.22f, 13.45f, -0.17f);
         ChatManager.TutorialChat("\nThis green box is a Medkit, it will restore damage to your suit.\n\nThey can be picked up by touching them."
@@ -435,4 +513,47 @@ public class TutorialInstructions : MonoBehaviour {
         yield return new WaitForSeconds(60);
         ChatManager.TutorialChat("\nWhen you're ready to end the simulation, press Escape.");
     }
+	
+	//checklist funstions
+	private int listItemCount = 0;
+	private bool[] checks;
+	private string[] listitems;
+	void CheckList(int itemcount){
+		listItemCount = itemcount;
+		checks = new bool[9];
+		listitems = new string[9];
+	}
+	void updateCheckList(int itemnum, bool ticked, string itemtext = "itemtext"){
+		checks[itemnum] = ticked;
+		if(!ticked){
+			listitems[itemnum] = itemtext;
+		}
+	}
+	private int listHeight;
+	private bool untickedCheck = false;
+	private bool tickedCheck = true;
+	private int standardX = 25;
+	private int standardY = 80;
+	private int Ydeviation = 0;
+	private Color tmpColor = new Color(1.0f,1.0f,1.0f,1.0f);
+	void OnGUI(){
+		tmpColor = GUI.color;
+		GUI.color = new Color(1.0f,1.0f,1.0f,5.0f);
+		GUI.enabled = false;
+		if(listItemCount > 0){
+			listHeight = listItemCount*35;
+			GUI.Box(new Rect(standardX-5, standardY-5, 200, listHeight), " ", "box");
+			Ydeviation = 0;
+			for(int i = 1; i <= listItemCount; i++){
+				if(checks[i]){
+					tickedCheck = GUI.Toggle(new Rect(standardX, standardY+Ydeviation, 190, 30), tickedCheck, listitems[i]);
+				} else {
+					untickedCheck = GUI.Toggle(new Rect(standardX, standardY+Ydeviation, 190, 30), untickedCheck, listitems[i]);
+				}
+				Ydeviation = i*35;
+			}
+		}
+		GUI.enabled = true;
+		GUI.color = tmpColor;
+	}
 }
