fc64a4857f6eef313b47226ea42d9d76ee32fdd3
Schwenckenator <matt.schwencke@gmail.com>
2015-08-12 10:02:57 +1000

    Started to move code to different files.
    Stopping here, was doing stuff in the wrong order.

--- ZeroG/Assets/$Scripts/UI/Windows/Options/UIControlSettings.cs
+++ ZeroG/Assets/$Scripts/UI/Windows/Options/UIControlSettings.cs
@@ -1,15 +1,36 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class UIControlSettings : MonoBehaviour {
+    private InputField[] inputFields;
+    private Slider[] sliders;
+    private Toggle[] toggles;
 
 	// Use this for initialization
 	void Start () {
-	
+        sliders = GetComponentsInChildren<Slider>(true);
+        inputFields = GetComponentsInChildren<InputField>(true);
+        toggles = GetComponentsInChildren<Toggle>(true);
+
+        SliderUpdate();
+        InputFieldUpdate();
+        ToggleUpdate();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+
+    private void ToggleUpdate() {
+        toggles[0].isOn = (SettingsManager.instance.MouseYDirection == 1);
+    }
+
+    private void InputFieldUpdate() {
+        inputFields[0].text = SettingsManager.instance.MouseSensitivityX.ToString();
+        inputFields[1].text = SettingsManager.instance.MouseSensitivityY.ToString();
+    }
+
+    private void SliderUpdate() {
+        sliders[0].value = SettingsManager.instance.MouseSensitivityX * 100;
+        sliders[1].value = SettingsManager.instance.MouseSensitivityY * 100;
+    }
+
+
 }
