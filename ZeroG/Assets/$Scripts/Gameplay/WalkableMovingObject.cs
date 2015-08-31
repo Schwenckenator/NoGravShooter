d8a9aa9390168763c1fd9cbdb8ebcfa9358ab033
Schwenckenator <matt.schwencke@gmail.com>
2015-08-31 17:34:38 +1000

    Floating objects now freeze movement.
    Works okay now. There is still some weird rotation going on, but I don't
    think I can fix that without some finagling.

--- ZeroG/Assets/$Scripts/Gameplay/WalkableMovingObject.cs
+++ ZeroG/Assets/$Scripts/Gameplay/WalkableMovingObject.cs
@@ -3,31 +3,74 @@ using System.Collections;
 
 public class WalkableMovingObject : MonoBehaviour {
 
-	void OnCollisionEnter(Collision info){
+    //void OnCollisionEnter(Collision info){
+    //    Debug.Log("Collision Enter! " + Time.time.ToString());
+    //    if (info.collider.CompareTag("Player") && info.collider.GetComponent<NetworkView>().isMine) {
+    //        Debug.Log("Set the Actor! " + Time.time.ToString());
+    //        actor = info.collider.transform;
+    //    }
+    //}
+
+    //void OnCollisionExit(Collision info){
+    //    Debug.Log("Collision exit. " + Time.time.ToString());
+    //    if(info.collider.CompareTag("Player") && info.collider.GetComponent<NetworkView>().isMine){
+    //        Debug.Log("Unset actor. " + Time.time.ToString());
+    //        actor = null;
+    //    }
+    //}
+
+    //Transform actor;
+    //Vector3 oldPosition;
+    
+    //void FixedUpdate() {
+    //    if (actor != null) {
+    //        Debug.Log("Moved the actor. " + Time.time.ToString());
+    //        Vector3 translation = transform.position - oldPosition;
+    //        //Vector3 translation = oldPosition - transform.position;
+    //        actor.transform.Translate(translation);
+    //    }
+    //    oldPosition = transform.position;
+    //}
+    void OnCollisionEnter(Collision info) {
         Debug.Log("Collision Enter! " + Time.time.ToString());
         if (info.collider.CompareTag("Player") && info.collider.GetComponent<NetworkView>().isMine) {
-            Debug.Log("Set the Actor! " + Time.time.ToString());
+            
             actor = info.collider.transform;
-		}
-	}
+            actor.GetComponent<KeyboardInput>().canWalk = false;
+            actor.SetParent(transform);
 
-	void OnCollisionExit(Collision info){
-        Debug.Log("Collision exit. " + Time.time.ToString());
-		if(info.collider.CompareTag("Player") && info.collider.GetComponent<NetworkView>().isMine){
-            Debug.Log("Unset actor. " + Time.time.ToString());
-            actor = null;
-		}
-	}
+            localPos = actor.localPosition;
+        }
+    }
+
+    //void OnCollisionExit(Collision info) {
+    //    Debug.Log("Collision exit. " + Time.time.ToString());
+    //    if (info.collider.CompareTag("Player") && info.collider.GetComponent<NetworkView>().isMine) {
+    //        FreeActor();
+    //    }
+    //}
+
+    private void FreeActor() {
+        actor.SetParent(null);
+        actor.GetComponent<KeyboardInput>().canWalk = true;
+        actor = null;
+    }
 
     Transform actor;
+    Vector3 localPos;
     Vector3 oldPosition;
-    
+
     void FixedUpdate() {
         if (actor != null) {
-            Debug.Log("Moved the actor. " + Time.time.ToString());
-            Vector3 translation = transform.position - oldPosition;
-            //Vector3 translation = oldPosition - transform.position;
-            actor.transform.Translate(translation);
+            if(InputConverter.GetKey(KeyBind.JetUp)){
+                FreeActor();
+            } else {
+                Debug.Log("Moved the actor. " + Time.time.ToString());
+                actor.localPosition = localPos;
+            }
+            
+            //Vector3 translation = transform.position - oldPosition;
+            //actor.transform.Translate(translation);
         }
         oldPosition = transform.position;
     }
