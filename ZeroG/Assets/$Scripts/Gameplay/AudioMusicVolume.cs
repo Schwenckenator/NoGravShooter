ca21a726ca13aa28c5cb3707c88fbcb0e13b4cdf
Nathan <nathanjbrown@live.com>
2016-03-13 01:32:55 +1000

    Implemented new music into the game
    Music runs on a shuffle system, soundtrack list is found on the camera
    of each scene, tracks can be set on a by-scene basis potentially giving
    each map its own track or track list.

--- ZeroG/Assets/$Scripts/Gameplay/AudioMusicVolume.cs
+++ ZeroG/Assets/$Scripts/Gameplay/AudioMusicVolume.cs
@@ -3,6 +3,9 @@ using System.Collections;
 
 public class AudioMusicVolume : MonoBehaviour {
     private static AudioSource source;
+	public AudioClip[] soundtrack;
+	private int currenttrack;
+	private int prevtrack;
 	// Use this for initialization
     void Awake() {
         source = GetComponent<AudioSource>();
@@ -10,10 +13,29 @@ public class AudioMusicVolume : MonoBehaviour {
 
     void Start() {
         UpdateMusicVolume();
-        source.Play();
+		if (soundtrack.Length > 1){
+			source.clip = soundtrack[Random.Range(0, soundtrack.Length)];
+		} else {
+			source.clip = soundtrack[0];
+		}
+		source.Play();
     }
+	
+	void Update (){
+		if (!source.isPlaying){
+			if (soundtrack.Length > 1){
+				do{
+					currenttrack = Random.Range(0, soundtrack.Length);
+					source.clip = soundtrack[currenttrack];
+					source.Play();
+				}while(currenttrack == prevtrack);
+			} else {
+				source.Play();
+			}
+		}
+	}
 
     public static void UpdateMusicVolume() {
-        source.volume = SettingsManager.instance.VolumeMusic;
+		source.volume = SettingsManager.instance.VolumeMusic;
     }
 }
