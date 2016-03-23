a1f8b6e6dbe067464558e3dbd8f3827c07ac549b
Schwenckenator <matt.schwencke@gmail.com>
2016-03-24 00:15:32 +1000

    Added Ready toggle to player list entries

--- ZeroG/Assets/$Scripts/UI/PlayerListEntry.cs
+++ ZeroG/Assets/$Scripts/UI/PlayerListEntry.cs
@@ -4,13 +4,30 @@ using System.Collections;
 
 public class PlayerListEntry : MonoBehaviour {
     public Player myPlayer;
-    public Text myText;
+    public Text myNameText;
+    public Text myReadyText;
 	// Use this for initialization
 	void Start () {
         UpdateName();   
 	}
 	
     public void UpdateName() {
-        myText.text = myPlayer.Name;
+        myNameText.text = myPlayer.Name;
+    }
+
+    public void UpdateToggle(bool ready) {
+        UpdateReadyText(ready);
+    }
+
+    private void UpdateReadyText(bool isReady) {
+        if (isReady) {
+            myReadyText.text = "Ready!";
+        } else {
+            if (myPlayer.isMine) { // Is the player the local client
+                myReadyText.text = "Ready?";
+            } else {
+                myReadyText.text = "";
+            }
+        }
     }
 }
